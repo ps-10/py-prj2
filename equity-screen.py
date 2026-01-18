65aaa8ae814b3270852f88d26284195c563990b4
Pradeep S <132168277+ps-10@users.noreply.github.com>
2026-01-18 12:58:12 -0600

    1/18: changed equity screening code .. 1)got new dataset from kaggle for sp500 companies, 2) changed header names to match new dataset.

--- equity-screen.py
+++ equity-screen.py
@@ -2,31 +2,36 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# create a list of companies less than the  PE ratio specified by the user & see a histogram distribution of the same
+# create a list of companies less than the  PE ratio specified by the user ..
+# & see a histogram distribution of the same. data downloaded from kaggle.
 
 #-------------
 pd.set_option('display.max_columns', None)  # to not truncate columns
+pd.set_option('display.max_rows', None)  # to not truncate rows
 
 #------------- data load, cleanup
 file1 = r"C:\Users\prade\PycharmProjects\py-prj2\PE.csv"  # read source data with PE values
 df1 = pd.read_csv(file1)  # store to dataframe
-df1['PE']= round(df1['PE'],2)  # round all PE ratios to 2 decimals
+df1['Price/Earnings']= round(df1['Price/Earnings'],2)  # round all PE ratios to 2 decimals
 print(df1.head())  # check if file load was correct
 
 #-------------- user prompts
-country = input("Enter your company origin name: ")  # user input for company origin name
+sector = input("Enter your sector name: ")  # user input for sector name
 max_PE = round(float(input("Enter your max PE: ")),2)  # user input for max PE
 
 #-------------- data filters
-filtered_df1 = df1[(df1['Company Origin'] == country) & (df1['PE'] < max_PE)]  # filter the dataframe
+condition = (df1['Sector'] == sector) & (df1['Price/Earnings'] < max_PE)  # create a variable condition to store all conditions
+filtered_df1 = df1[condition]  # filter the dataframe based on conditions
 print(filtered_df1.head())  # check if filtering worked
-filtered_df1 = filtered_df1.sort_values(by='PE', ascending=False)  # sort df by descending PE
-file2 = r"C:\Users\prade\PycharmProjects\py-prj2\PE_filtered.csv"  # file path for the new filtered records
+filtered_df1 = filtered_df1.sort_values(by='Price/Earnings', ascending=False)  # sort df by descending PE
+file2 = r"C:\Users\prade\PycharmProjects\py-prj2\PE_filtered.csv"
+    # file path for the new filtered records
 filtered_df1.to_csv(file2, index=False)   # write filtered records to a new file
 
 #-------------- data plots (histogram)
-my_bins = [0, 3, 7, 12]  # i defined the bin sizes
-sns.histplot(data = filtered_df1, x = 'PE', bins = my_bins, color = 'blue')  # plot the filtered df on a histogram
+my_bins = [0, 3, 7, 11, 15, 19, 23]  # i defined the bin sizes
+sns.histplot(data = filtered_df1, x = 'Price/Earnings', bins = my_bins)
+    # plot the filtered df on a histogram
 plt.xlabel('PE')
 plt.ylabel('Count')
 plt.title('PE Distribution')
